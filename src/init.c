1f05c27105bcecb52f1c34be8ce27a4c63ab9d72
Xiao Li <xiaoli@mcw.edu>
2021-02-03 21:46:24 -0600

    init

---
+++ src/init.c
@@ -0,0 +1,22 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+//extern SEXP cgbmm(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP cplbart(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+//    {"cgbmm",   (DL_FUNC) &cgbmm,   34}, 
+  {"cplbart",(DL_FUNC) &cplbart, 31},
+/*    {"cdpmbart",(DL_FUNC) &cdpmbart,25},
+    {"cdpmwbart",(DL_FUNC) &cdpmwbart,27}, */
+    {NULL, NULL, 0}
+};
+
+void R_init_BART(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
